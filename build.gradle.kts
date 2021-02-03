b70d0b3e0c425d170b618ed0ac20f4fbeededb52
sdercolin <sder.colin@gmail.com>
2021-02-04 03:21:55 +0900

    Upgrade Kotlin and Gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,63 +1,70 @@
 plugins {
-    id("org.jetbrains.kotlin.js") version "1.3.71"
-    kotlin("plugin.serialization") version "1.3.71"
+    kotlin("js") version "1.4.30"
+    kotlin("plugin.serialization") version "1.4.30-RC"
 }
 
 group = "com.sdercolin.utaformatix"
 
 repositories {
-    maven("https://kotlin.bintray.com/kotlin-js-wrappers/")
     mavenCentral()
+    maven { url = uri("https://kotlin.bintray.com/kotlin-js-wrappers") }
     jcenter()
 }
 
 dependencies {
     // Kotlin
     implementation(kotlin("stdlib-js"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.3.7")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-js:0.20.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.4.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
 
     // React, React DOM + Wrappers
-    implementation("org.jetbrains:kotlin-react:16.13.0-pre.94-kotlin-1.3.70")
-    implementation("org.jetbrains:kotlin-react-dom:16.13.0-pre.94-kotlin-1.3.70")
+    implementation("org.jetbrains:kotlin-react:16.13.1-pre.113-kotlin-1.4.0")
+    implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.113-kotlin-1.4.0")
+    implementation("org.jetbrains:kotlin-styled:1.0.0-pre.113-kotlin-1.4.0")
     implementation(npm("react", "16.13.1"))
     implementation(npm("react-dom", "16.13.1"))
-    implementation("org.jetbrains:kotlin-styled:1.0.0-pre.94-kotlin-1.3.70")
-    implementation(npm("styled-components"))
-    implementation(npm("inline-style-prefixer"))
 
     // React components
-    implementation(npm("@material-ui/core"))
-    implementation(npm("@material-ui/icons"))
-    implementation(npm("@material-ui/lab", "4.0.0-alpha.56"))
-    implementation(npm("react-file-drop"))
-    implementation(npm("react-is"))
-    implementation(npm("react-markdown"))
+    implementation(npm("@material-ui/core", "4.11.3"))
+    implementation(npm("@material-ui/icons", "4.11.2"))
+    implementation(npm("@material-ui/lab", "4.0.0-alpha.57"))
+    implementation(npm("react-file-drop", "3.1.2"))
+    implementation(npm("react-is", "17.0.1"))
+    implementation(npm("react-markdown", "5.0.3"))
 
     // Localization
-    implementation(npm("i18next"))
-    implementation(npm("react-i18next"))
-    implementation(npm("i18next-browser-languagedetector"))
+    implementation(npm("i18next", "19.8.7"))
+    implementation(npm("react-i18next", "11.8.5"))
+    implementation(npm("i18next-browser-languagedetector", "6.0.1"))
 
     // Others
-    implementation(npm("jszip"))
-    implementation(npm("file-saver"))
-    implementation(npm("raw-loader"))
-    implementation(npm("file-loader"))
-    implementation(npm("encoding-japanese"))
-    implementation(npm("uuid"))
-    implementation(npm("midi-parser-js"))
+    implementation(npm("jszip", "3.5.0"))
+    implementation(npm("file-saver", "2.0.5"))
+    implementation(npm("raw-loader", "4.0.2"))
+    implementation(npm("file-loader", "6.2.0"))
+    implementation(npm("encoding-japanese", "1.0.30"))
+    implementation(npm("uuid", "8.3.2"))
+    implementation(npm("midi-parser-js", "4.0.4"))
 
     // Testing
     testImplementation(kotlin("test-js"))
 }
 
-kotlin.target {
-    browser {
-        testTask {
-            useKarma {
-                useChromeHeadless()
-                useFirefox()
+kotlin {
+    js(LEGACY) {
+        browser {
+            binaries.executable()
+            webpackTask {
+                cssSupport.enabled = true
+            }
+            runTask {
+                cssSupport.enabled = true
+            }
+            testTask {
+                useKarma {
+                    useChromeHeadless()
+                    webpackConfig.cssSupport.enabled = true
+                }
             }
         }
     }
