b6324255ab14d99139758ae31e6f2495db89a21f
shine_5402 <shine_5402@live.cn>
2021-07-30 22:56:20 +0800

    resample method implemented

---
+++ src/main/kotlin/process/Resample.kt
@@ -0,0 +1,19 @@
+package process
+
+import model.Pitch
+
+fun List<Pair<Long, Double?>>.resampled(sampleRate : Long,
+                                      interpolateMethod: (prev: Pair<Long, Double?>?, next: Pair<Long, Double?>?,
+                                                          pos: Long) -> Double?): MutableList<Pair<Long, Double?>> {
+        val result: MutableList<Pair<Long, Double?>> = mutableListOf()
+        val length = this.map { it.first }.maxOrNull() ?: 0
+        for (current in 0..length step sampleRate){
+            val prev = this.lastOrNull{it.first <= current}
+            val next = this.firstOrNull{it.first >= current}
+            result.add(Pair(current, interpolateMethod(prev, next, current)))
+        }
+        return result
+    }
+
+/// A shorthand for pitch represented in dot. Its interpolateMethod simply copy the value from prev.
+fun Pitch.dotResampled(sampleRate: Long) = Pitch(data.resampled(sampleRate) { prev, _, _ -> prev?.second }, isAbsolute)
