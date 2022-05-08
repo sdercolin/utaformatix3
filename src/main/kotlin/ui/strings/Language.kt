c51ec4ee5177580f360a97b56cd32ddacf4637e3
Exorcism <46537034+Exorcism0666@users.noreply.github.com>
2022-05-08 11:04:30 +0200

    Update Language.kt

--- src/main/kotlin/ui/strings/Language.kt
+++ src/main/kotlin/ui/strings/Language.kt
@@ -3,5 +3,6 @@ package ui.strings
 enum class Language(val code: String, val displayName: String) {
     English("en", "English"),
     Japanese("ja", "日本語"),
-    SimplifiedChinese("zh-CN", "简体中文")
+    SimplifiedChinese("zh-CN", "简体中文"),
+    French("fr", "Français")
 }
