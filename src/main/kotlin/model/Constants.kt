47c0f480133e96d8f5e415c61e7f4d2655b8c1a2
sdercolin <sder.colin@gmail.com>
2022-06-06 20:53:20 +0900

    Accept note without pitch tag when importing musicxml

--- src/main/kotlin/model/Constants.kt
+++ src/main/kotlin/model/Constants.kt
@@ -7,6 +7,7 @@ const val DEFAULT_LYRIC = "あ"
 const val DEFAULT_BPM = 120.0
 const val DEFAULT_METER_HIGH = 4
 const val DEFAULT_METER_LOW = 4
+const val DEFAULT_KEY = 60
 const val KEY_CENTER_C = 60.0
 const val LOG_FRQ_CENTER_C = 5.566914341
 const val LOG_FRQ_DIFF_ONE_KEY = 0.05776226505
