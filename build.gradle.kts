09281bd2d484e3341f378a0f0921b9d9da2684bb
sdercolin <sder.colin@gmail.com>
2022-06-09 17:42:21 +0900

    Update Kotlin, React and Mui

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("js") version "1.4.30"
-    kotlin("plugin.serialization") version "1.4.30-RC"
+    kotlin("js") version "1.6.21"
+    kotlin("plugin.serialization") version "1.6.21"
     id("org.jlleitschuh.gradle.ktlint") version "9.4.1"
 }
 
@@ -18,20 +18,17 @@ configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
 dependencies {
     // Kotlin
     implementation(kotlin("stdlib-js"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.4.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:1.6.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
 
     // React, React DOM + Wrappers
-    implementation("org.jetbrains:kotlin-react:17.0.1-pre.148-kotlin-1.4.21")
-    implementation("org.jetbrains:kotlin-react-dom:17.0.1-pre.148-kotlin-1.4.21")
-    implementation("org.jetbrains:kotlin-styled:5.2.1-pre.148-kotlin-1.4.21")
-    implementation(npm("react", "17.0.2"))
-    implementation(npm("react-dom", "17.0.2"))
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react:17.0.2-pre.290-kotlin-1.6.10")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:17.0.2-pre.290-kotlin-1.6.10")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-css:17.0.2-pre.290-kotlin-1.6.10")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-mui:5.8.0-pre.343")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-mui-icons:5.8.0-pre.343")
 
     // React components
-    implementation(npm("@material-ui/core", "4.11.4"))
-    implementation(npm("@material-ui/icons", "4.11.2"))
-    implementation(npm("@material-ui/lab", "4.0.0-alpha.58"))
     implementation(npm("react-file-drop", "3.1.2"))
     implementation(npm("react-is", "17.0.2"))
     implementation(npm("react-markdown", "5.0.3"))
@@ -53,24 +50,15 @@ dependencies {
     implementation(npm("js-yaml", "4.1.0"))
 
     // Testing
-    testImplementation(kotlin("test-js"))
+    testImplementation(kotlin("test"))
 }
 
 kotlin {
     js(LEGACY) {
         browser {
             binaries.executable()
-            webpackTask {
+            commonWebpackConfig {
                 cssSupport.enabled = true
-            }
-            runTask {
-                cssSupport.enabled = true
-            }
-            testTask {
-                useKarma {
-                    useChromeHeadless()
-                    webpackConfig.cssSupport.enabled = true
-                }
             }
         }
     }
