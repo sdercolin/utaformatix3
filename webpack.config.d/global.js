f0811bff4255e00479007a0d7124341f5a7fd7df
sdercolin <sder.colin@gmail.com>
2022-06-09 22:32:57 +0900

    Fix js for karma

--- webpack.config.d/global.js
+++ webpack.config.d/global.js
@@ -1,2 +1,4 @@
-config.output.globalObject = `(typeof self !== 'undefined' ? self : this)`
+if(typeof config.output !== 'undefined') {
+    config.output.globalObject = `(typeof self !== 'undefined' ? self : this)`
+}
 config.resolve.fallback = { "stream": require.resolve("stream-browserify"), "buffer": require.resolve("buffer/") }
