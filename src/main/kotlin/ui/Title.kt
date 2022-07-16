89494c6fdf87e9afbf508f9ff00142ba2301f587
sdercolin <sder.colin@gmail.com>
2022-07-17 01:57:27 +0900

    Fix Title

--- src/main/kotlin/ui/Title.kt
+++ src/main/kotlin/ui/Title.kt
@@ -1,21 +1,18 @@
 package ui
 
-import csstype.Margin
 import csstype.px
+import kotlinx.js.jso
 import mui.material.Typography
 import mui.material.styles.TypographyVariant
 import react.ChildrenBuilder
-import react.css.css
-import react.dom.html.ReactHTML.div
 import ui.strings.Strings
 import ui.strings.string
 
-fun ChildrenBuilder.title(titleKey: Strings) = div {
-    css {
-        Margin(vertical = 20.px, horizontal = 0.px)
+fun ChildrenBuilder.title(titleKey: Strings) = Typography {
+    style = jso {
+        marginTop = 30.px
+        marginBottom = 20.px
     }
-    Typography {
-        variant = TypographyVariant.h3
-        +string(titleKey)
-    }
+    variant = TypographyVariant.h3
+    +string(titleKey)
 }
