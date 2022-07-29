5b8317b103b73cde7dc31968b262ff190d805d20
sdercolin <sder.colin@gmail.com>
2022-07-29 16:27:03 +0900

    Reformat js code

--- webpack.config.d/global.js
+++ webpack.config.d/global.js
@@ -1,4 +1,4 @@
-if(typeof config.output !== 'undefined') {
+if (typeof config.output !== 'undefined') {
     config.output.globalObject = `(typeof self !== 'undefined' ? self : this)`
 }
-config.resolve.fallback = { "stream": require.resolve("stream-browserify"), "buffer": require.resolve("buffer/") }
+config.resolve.fallback = {"stream": require.resolve("stream-browserify"), "buffer": require.resolve("buffer/")}
