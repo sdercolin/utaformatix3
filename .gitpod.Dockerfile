ef39eb2cf81743b19cfb317f899a8936140d2972
oxygen-dioxide <1463567152@qq.com>
2022-09-26 00:59:02 +0000

    de-gitpod

--- .gitpod.Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM gitpod/workspace-full
-
-USER gitpod
-RUN brew install kotlin
--- webpack.config.d/global.js
+++ webpack.config.d/global.js
@@ -2,9 +2,3 @@ if (typeof config.output !== 'undefined') {
     config.output.globalObject = `(typeof self !== 'undefined' ? self : this)`
 }
 config.resolve.fallback = {"stream": require.resolve("stream-browserify"), "buffer": require.resolve("buffer/")}
-config.devServer = Object.assign(
-    {},
-    config.devServer || {},
-    { host: '0.0.0.0',
-    allowedHosts: ['localhost', '.gitpod.io'], }
-)
