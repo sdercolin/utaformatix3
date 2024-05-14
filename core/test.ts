ce892cebe776aae2b256a9480fcc7df5dd36b10d
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-05-14 22:03:55 +0900

    Add: Add test

---
+++ core/test.ts
@@ -0,0 +1,32 @@
+import { copy } from "jsr:@std/fs@^0.224.0";
+import { $ } from "jsr:@david/dax@0.41.0";
+import { build, emptyDir } from "jsr:@deno/dnt@^0.41.1";
+
+$.setPrintCommand(true);
+
+const testPackageRoot =
+  `${import.meta.dirname}/../build/temporary_test_package`;
+
+await emptyDir(testPackageRoot);
+console.log("Testing on Deno...");
+await $`deno test -A`;
+
+console.log("Building and testing for npm...");
+await build({
+  entryPoints: ["./mod.ts"],
+  outDir: testPackageRoot,
+  shims: {
+    deno: true,
+  },
+  package: {
+    name: "utaformatix",
+    version: "0.0.0",
+  },
+  async postBuild() {
+    await copy(
+      "./testAssets",
+      testPackageRoot + "/esm/testAssets",
+    );
+  },
+  scriptModule: false,
+});
