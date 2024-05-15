6be6ef312cf82f63c1e324318817d139e561119c
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-05-15 19:16:53 +0900

    Add: Add 10 tracks test

--- core/packaged/mod.test.ts
+++ core/packaged/mod.test.ts
@@ -1,5 +1,7 @@
 import * as uf from "./mod.ts";
 import { expandGlob } from "jsr:@std/fs@^0.224.0/expand-glob";
+import { assertEquals } from "jsr:@std/assert@^0.224.0";
+const testAssetsDir = `${import.meta.dirname}/testAssets`;
 
 const parserMap = [
   ["ccs", "parseCcs"],
@@ -25,7 +27,7 @@ for await (const file of expandGlob("./testAssets/**/*")) {
   }
 
   const [, parse] = parser;
-  const path = file.path.replace(`${import.meta.dirname}/testAssets`, ".");
+  const path = file.path.replace(testAssetsDir, ".");
 
   Deno.test(`parse: ${path} using ${parse}`, async () => {
     const data = await Deno.readFile(file.path);
@@ -33,23 +35,46 @@ for await (const file of expandGlob("./testAssets/**/*")) {
   });
 }
 
-const stdMidiData = await Deno.readFile("./testAssets/standard.mid");
-const result = await uf.parseStandardMid(stdMidiData);
+Deno.test("parse: MultipleGenerateFunctions can serialize score with correct order", async () => {
+  const with10Tracks = await Deno.readFile(testAssetsDir + "/10tracks.svp");
+  const ufdata = await uf.parseSvp(with10Tracks);
 
-for (const name of [
-  "generateCcs",
-  "generateDv",
-  "generateMusicXml",
-  "generateStandardMid",
-  "generateSvp",
-  "generateUst",
-  "generateUstx",
-  "generateVocaloidMid",
-  "generateVpr",
-  "generateVsq",
-  "generateVsqx",
-] as const) {
+  const usts = await uf.generateUst(ufdata);
+  const noteNums = usts.map((ust) => {
+    const decoded = new TextDecoder().decode(ust);
+    const noteNum = decoded.match(/NoteNum=(\d+)/g);
+    if (!noteNum) {
+      throw new Error("No NoteNum found");
+    }
+    return parseInt(noteNum[0].split("=")[1]);
+  });
+
+  assertEquals(
+    noteNums,
+    // C4 to D5
+    [60, 62, 64, 65, 67, 69, 71, 72, 74, 76],
+  );
+});
+
+for (
+  const name of [
+    "generateCcs",
+    "generateDv",
+    "generateMusicXml",
+    "generateStandardMid",
+    "generateSvp",
+    "generateUst",
+    "generateUstx",
+    "generateVocaloidMid",
+    "generateVpr",
+    "generateVsq",
+    "generateVsqx",
+  ] as const
+) {
   Deno.test(`generate: ${name}`, async () => {
+    const stdMidiData = await Deno.readFile(testAssetsDir + "/standard.mid");
+    const result = await uf.parseStandardMid(stdMidiData);
+
     await uf[name](result);
   });
 }
