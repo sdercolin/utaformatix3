b1925c849ab1ad2524ed1407fb79fa4256ee85fa
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-05-17 18:29:54 +0900

    Delete: Delete deno things

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -27,19 +27,3 @@ kotlin {
         }
     }
 }
-
-val buildLibrary by tasks.register<Exec>("buildLibrary") {
-    dependsOn("browserProductionLibraryDistribution")
-
-    workingDir("$projectDir")
-    commandLine("deno", "run", "-A", "build.ts")
-}
-
-val testLibrary by tasks.register<Exec>("testLibrary") {
-    dependsOn(buildLibrary)
-    workingDir("$projectDir")
-    commandLine("deno", "run", "-A", "test.ts")
-}
-tasks.named("test") {
-    finalizedBy(testLibrary)
-}
--- core/packaged/mod.ts
+++
@@ -1,3 +0,0 @@
-export * from "./project.ts";
-export type { UfData } from "npm:utaformatix-data@^1.1.0";
-export { type JapaneseLyricsType, parseFunctions } from "./base.ts";
