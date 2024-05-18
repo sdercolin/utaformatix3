23ac6e5ebe81933cb5dd5ccbf0ec879c2dda3ba8
sevenc-nanashi <sevenc7c@sevenc7c.com>
2024-05-18 15:11:54 +0900

    Change: Use KDoc

--- core/src/main/kotlin/core/model/ProjectContainer.kt
+++ core/src/main/kotlin/core/model/ProjectContainer.kt
@@ -1,6 +1,8 @@
 package core.model
 
-// Container for a Document object for better typing (otherwise Document will be any in .d.ts)
+/**
+ * Container for a Project object for better typing (otherwise Document will be any in .d.ts)
+ */
 @OptIn(ExperimentalJsExport::class)
 @JsExport
-class ProjectContainer(@Suppress("NON_EXPORTABLE_TYPE") internal val project: Project)
+class ProjectContainer(internal val project: Project)
